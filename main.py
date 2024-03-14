1fd939c6d0942af35c9e045a250546daf81fe143
Marzio Reitano <marzioreitano@gmail.com>
2024-03-14 11:51:11 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import *
+
+if __name__ == '__main__':
+
+    # Read the environment
+    read_env()
+
+    # Set up the useful directories and check that files exist
+    setup_logger()
+
+    logging.info("Environment setup complete")
---
+++ utils.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+
+def read_env():
+    load_dotenv()
+
+
+def setup_logger():
+    logging.basicConfig(filename=os.path.join(os.getenv("LOGS"), f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"),
+                        level=logging.DEBUG,
+                        format="%(asctime)s - %(levelname)s: %(message)s",
+                        force=True)
