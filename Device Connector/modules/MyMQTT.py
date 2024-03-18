1b00fb17432bacf358f8799a17512b7438561ead
Marzio Reitano <marzioreitano@gmail.com>
2024-03-18 19:45:30 +0100

    Re-organized structure Device Connector.

--- Device Connector/modules/MyMQTT.py
+++ Device Connector/modules/MyMQTT.py
@@ -1,3 +1,4 @@
+import json
 import paho.mqtt.client as PahoMQTT
 
 
@@ -7,13 +8,10 @@ class MyMQTT:
         self.port = port
         self.notifier = notifier
         self.clientID = clientID
-
         self._topic = ""
         self._isSubscriber = False
-
         # create an instance of paho.mqtt.client
-        self._paho_mqtt = PahoMQTT.Client(clientID, False)
-
+        self._paho_mqtt = PahoMQTT.Client(clientID, True)
         # register the callback
         self._paho_mqtt.on_connect = self.myOnConnect
         self._paho_mqtt.on_message = self.myOnMessageReceived
@@ -26,25 +24,26 @@ class MyMQTT:
         self.notifier.notify(msg.topic, msg.payload)
 
     def myPublish(self, topic, msg):
-        # if needed, you can do some computation or error-check before publishing
-        print("publishing '%s' with topic '%s'" % (msg, topic))
         # publish a message with a certain topic
-        self._paho_mqtt.publish(topic, msg, 2)
+        self._paho_mqtt.publish(topic, json.dumps(msg), 2)
 
     def mySubscribe(self, topic):
-        # if needed, you can do some computation or error-check before subscribing
-        print("subscribing to %s" % topic)
         # subscribe for a topic
         self._paho_mqtt.subscribe(topic, 2)
-
         # just to remember that it works also as a subscriber
         self._isSubscriber = True
         self._topic = topic
+        print("subscribed to %s" % topic)
 
     def start(self):
         # manage connection to broker
         self._paho_mqtt.connect(self.broker, self.port)
         self._paho_mqtt.loop_start()
+
+    def unsubscribe(self):
+        if self._isSubscriber:
+            # remember to unsubscribe if it is working also as subscriber
+            self._paho_mqtt.unsubscribe(self._topic)
 
     def stop(self):
         if self._isSubscriber:
---
+++ Device Connector/sensors/DHT11.py
@@ -0,0 +1,43 @@
+from modules.data_processor import *
+import matplotlib.pyplot as plt
+
+
+class DHT11:
+
+    def __init__(self, path):
+        self.path = path
+        self.df = self.read_data()
+
+    def import_data(self):
+        data = pd.read_csv(self.path)
+        return data
+
+    def read_data(self):
+        data = self.import_data()
+        dataframe = dht11_sensor(data)
+        return dataframe
+
+    def plot_dataframe(self):
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.df.index, self.df['humidity'], color='blue', linestyle='-', label='Humidity')
+        plt.plot(self.df.index, self.df['temperature'], color='red', linestyle='-', label='Temperature')
+        plt.title('Humidity and temperature trends')
+        plt.xlabel('Date')
+        plt.ylabel('Value')
+        plt.xticks(rotation=45)
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+    def change_granularity(self, hours):
+        df = correct_non_numeric_values(self.df)
+        self.df = resample_dataframe(df, hours)
+
+
+if __name__ == '__main__':
+    path = '../data/dataset/GreenhouseClimate.csv'
+    sensor = DHT11(path)
+    sensor.change_granularity(1)
+    sensor.plot_dataframe()
+    print(sensor.df)
